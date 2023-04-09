aa7985c179fc82b428d8183be7a9bd1e55758272
Eikuld <28073955+DawnofSouls@users.noreply.github.com>
2023-04-08 18:25:09 -0700

    Added conditional statement to check whether can add number string or replace the number

--- Javascript.js
+++ Javascript.js
@@ -31,6 +31,11 @@ let OperandChoice;
 // Array Number to do the math
 let NumberList = [];
 
+//Add or Replace the number shown on screen. If AoR = 0, add number to value. For example, 
+// continuously pressing eight will get 8888. If AoR = 1, which only happens after you press an operation. In this case,
+// if you press 10 then press divide button and then 5, instead of 105, it will be 5
+let AoR = 0;
+
 // Automatically set time on upper right of an element
 let Time = document.getElementById("Time");
 Time.textContent = new Date().toLocaleTimeString().replace(/(.*)\D\d+/, '$1');
@@ -41,53 +46,113 @@ setInterval(TimeUpdate, 1000);
 
 //Onclick Number Functions
 Zero.addEventListener("click", () => {
-  InputNumber += "0";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "0";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 0;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 One.addEventListener("click", () => {
-  InputNumber += "1";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "1";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 1;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Two.addEventListener("click", () => {
-  InputNumber += "2";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "2";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 2;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Three.addEventListener("click", () => {
-  InputNumber += "3";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "3";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 3;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Four.addEventListener("click", () => {
-  InputNumber += "4";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "4";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 4;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Five.addEventListener("click", () => {
-  InputNumber += "5";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "5";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 5;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Six.addEventListener("click", () => {
-  InputNumber += "6";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "6";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 6;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Seven.addEventListener("click", () => {
-  InputNumber += "7";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "7";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 7;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Eight.addEventListener("click", () => {
-  InputNumber += "8";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "8";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 8;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 Nine.addEventListener("click", () => {
-  InputNumber += "9";
-  NumberScreen.textContent = Number(InputNumber);
+  if (AoR === 0) {
+    InputNumber += "9";
+    NumberScreen.textContent = Number(InputNumber);
+  } else if (AoR === 1) {
+    InputNumber = 9;
+    NumberScreen.textContent = Number(InputNumber);
+    AoR = 0;
+  }
 });
 
 //Onclick Tools Functions
@@ -98,10 +163,10 @@ Clear.addEventListener("click", () => {
   NumberScreen.textContent = Number(InputNumber);
 });
 
-PlusMinus.addEventListener("click", () => {
-  InputNumber = 0;
-  NumberScreen.textContent = Number(InputNumber);
-});
+// PlusMinus.addEventListener("click", () => {
+//   InputNumber = 0;
+//   NumberScreen.textContent = Number(InputNumber);
+// });
 
 Percentage.addEventListener("click", () => {
   let InitialValue = NumberScreen.textContent;
@@ -113,11 +178,17 @@ Percentage.addEventListener("click", () => {
 Divide.addEventListener("click", () => {
   NumberList.push(NumberScreen.textContent);
   OperandChoice = "Divide";
+  AoR = 1;
 });
 
 Equal.addEventListener("click", () => {
     if (OperandChoice === "Divide"){
+        NumberList.push(NumberScreen.textContent);
         let Answer = NumberList[0] / NumberList[1];
-        NumberScreen.textContent = Number(Answer);
+        if (Answer === Infinity){
+            Answer = "Error";
+        }
+        NumberScreen.textContent = Answer;
+        console.log(NumberList);
     }
   });
